5b803852a569e38763ef67ca121c83f62dfd3045
Felix Tripier <ftripier@gmail.com>
2015-02-24 17:03:08 -0800

    Orthographic coordinate parity

--- src/VertexShader.glsl
+++ src/VertexShader.glsl
@@ -4,26 +4,33 @@ const mat4 inverseMat = mat4(
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
 );
-vec4 clipspace(in vec4 pos) { 
+
+vec4 clipspace(vec4 pos) { 
     return vec4( 
-        ( ( (2.0 * pos.x) - resolution.x + size.x ) / resolution.x ) * (resolution.x/resolution.y),
+        ( (2.0 * pos.x) - resolution.x + size.x ) / resolution.x,
         ( (2.0 * pos.y) + resolution.y - size.y ) / resolution.y,
         pos.z,
-       1.0
+       0.0
     );
 }
-vec4 applyTransform(in vec4 pos) {
+
+vec4 applyTransform(vec4 pos) {
    mat4 projection = perspective;
-   float width = size.x/resolution.y;
-   float height = size.y/resolution.y;
-   pos.xy *= vec2(width, height);
-   projection[0][0] = 1.0/(resolution.x/resolution.y);
    pos.y *= -1.0;
+   pos.xyz *= size.xyz;
    mat4 MVMatrix = inverseMat * transform;
-   MVMatrix[3] = clipspace(MVMatrix[3]);
-   pos = perspective * MVMatrix * pos;
+   vec4 translation = clipspace(MVMatrix[3]);
+   MVMatrix[3] = vec4(0.0, 0.0, 0.0, 1.0);
+   projection[0][0] = 1.0/resolution.x;
+   projection[1][1] = 1.0/resolution.y;
+   projection[2][2] = (resolution.y > resolution.x) ? 1.0/resolution.y : 1.0/resolution.x;
+
+   pos = projection * MVMatrix * pos;
+   pos += translation;
+
    return pos;
 }
+
 // Main function of the vertex shader.  Passes texture coordinat
 // and normal attributes as varyings and passes the position
 // attribute through position pipeline
