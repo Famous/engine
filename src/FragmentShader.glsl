7a4aa3e352b6f2e9f0318113fd1a2ac77226c558
Farhad Ghayour <f@famo.us>
2015-02-26 16:22:00 -0800

    fix: Merge materials with glslify

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -1,6 +1,8 @@
+#pragma glslify: applyMaterial = require(./chunks/applyMaterial)
 #pragma glslify: lambertianLight = require(./chunks/lambertianLight)
 
 void main() {
-    vec3 color = lambertianLight(baseColor, vPosition, vNormal, u_LightPosition, u_LightColor);
+    vec3 material = baseColor.x >= 0.0 ? baseColor : applyMaterial(baseColor);
+    vec3 color = lambertianLight(material, vPosition, vNormal, u_LightPosition, u_LightColor);
     gl_FragColor = vec4(color, opacity);
 }
