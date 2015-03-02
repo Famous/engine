1d0d1973248af9418f37ff39551892765b69a537
Felix Tripier <ftripier@gmail.com>
2015-03-02 13:30:05 -0800

    Fix: Applies modelview with origin shift

--- src/VertexShader.glsl
+++ src/VertexShader.glsl
@@ -5,36 +5,38 @@
 #pragma glslify: transpose = require(./chunks/transpose)
 
 vec4 applyTransform(vec4 pos) {
-    mat4 projection = perspective;
-    mat4 MVMatrix = invertYAxis(transform);
-    vec4 translation = MVMatrix[3];
-    MVMatrix[3] = vec4(0.0, 0.0, 0.0, 1.0);
-    
-    pos.xyz *= size.xyz;
-    pos.y *= -1.0;
-    vec4 pixelPosition = vec4(pos.x * 0.5, pos.y * 0.5, pos.z, 1.0);
-    pixelPosition += vec4(size.x * 0.5, size.y * 0.5, 0.0, 0.0);
+   mat4 projection = perspective;
+   mat4 MVMatrix = invertYAxis(transform);
+   vec4 translation = MVMatrix[3];
+   MVMatrix[3] = vec4(0.0, 0.0, 0.0, 1.0);
+   
+   pos.xyz *= size.xyz;
+   pos.y *= -1.0;
+   vec4 pixelPosition = vec4(pos.x * 0.5, pos.y * 0.5, pos.z, 1.0);
+   mat4 pixelTransform = transform;
+   pixelTransform[3][0] += size.x * 0.5;
+   pixelTransform[3][1] += size.y * 0.5;
 
-    projection[0][0] = 1.0/resolution.x;
-    projection[1][1] = 1.0/resolution.y;
-    projection[2][2] = (resolution.y > resolution.x) ? -1.0/resolution.y : -1.0/resolution.x;
+   projection[0][0] = 1.0/resolution.x;
+   projection[1][1] = 1.0/resolution.y;
+   projection[2][2] = (resolution.y > resolution.x) ? -1.0/resolution.y : -1.0/resolution.x;
 
 
-    vPosition = (transform * pixelPosition).xyz;
+   vPosition = (pixelTransform * pixelPosition).xyz;
 
-    pos = projection * MVMatrix * pos;
+   pos = projection * MVMatrix * pos;
 
-    pos += convertToClipSpace(translation);
+   pos += convertToClipSpace(translation);
 
-    return pos;
+   return pos;
 }
 
 // Main function of the vertex shader.  Passes texture coordinat
 // and normal attributes as varyings and passes the position
 // attribute through position pipeline
 void main() {
-    gl_PointSize = 10.0;
-    vNormal = transpose(mat3(inverse(transform))) * normals;
-    vTextureCoordinate = texCoord;
-    gl_Position = applyTransform(vec4(pos, 1.0));
+   gl_PointSize = 10.0;
+   vNormal = transpose(mat3(inverse(transform))) * normals;
+   vTextureCoordinate = texCoord;
+   gl_Position = applyTransform(vec4(pos, 1.0));
 }
