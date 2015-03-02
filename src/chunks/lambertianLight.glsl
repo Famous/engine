783434871b666cdcece31071df03ea997b7fc8a6
Felix Tripier <ftripier@gmail.com>
2015-03-02 10:08:23 -0800

    Remove unnecessary cast

--- src/chunks/lambertianLight.glsl
+++ src/chunks/lambertianLight.glsl
@@ -1,6 +1,6 @@
 vec3 lambertianLight(vec3 baseColor, vec3 vPosition, vec3 vNormal, vec3 u_LightPosition, vec3 u_LightColor) {
     vec3 normal = normalize(vNormal);
-    vec3 lightDirection = normalize(vec3(u_LightPosition) - vec3(vPosition));
+    vec3 lightDirection = normalize(u_LightPosition - vPosition);
     float nDotL = max(dot(lightDirection, normal), 0.0);
     vec3 diffuse = u_LightColor * baseColor * nDotL;
     return diffuse;
