577763fb55358f8a7e235081a6e28f8cd77b14da
Alexander Gugel <alexander.gugel@gmail.com>
2015-03-05 10:54:43 -0800

    refact: Add Clock getters, noLongerUpdate function, cleanup

--- src/Clock.js
+++ src/Clock.js
@@ -2,38 +2,49 @@
 
 var GlobalDispatch = require('./GlobalDispatch');
 
-var FRAME = 'FRAME';
-
 function Clock () {
-    this.dispatch = new GlobalDispatch();
-    this.roots = [];
+    this._globalDispatch = new GlobalDispatch();
+    this._updates = [];
     var _this = this;
-    this.dispatch.targetedOn('engine', FRAME, function(time) {
+    this._globalDispatch.targetedOn('engine', 'FRAME', function (time) {
         _this.step(time);
     });
+    this._time = null;
 }
 
 Clock.prototype.step = function step (time) {
-    this.time = time;
+    this._time = time;
 
-    for (var i = 0, len = this.roots.length ; i < len ; i++)
-        this.roots[i].update(time);
+    for (var i = 0, len = this._updates.length ; i < len ; i++)
+        this._updates[i].update(time);
 
-    this.dispatch.flush();
+    this._globalDispatch.flush();
 };
 
 Clock.prototype.update = function update (target) {
-    this.roots.push(target);
+    this._updates.push(target);
+    return this;
+};
+
+Clock.prototype.noLongerUpdate = function noLongerUpdate(target) {
+    var index = this._updates.indexOf(target);
+    if (index > -1) {
+        this._updates.splice(index, 1);
+    }
     return this;
 };
 
 Clock.prototype.getTime = function getTime () {
-    return this.time;
+    return this._time;
 };
 
 Clock.prototype.receiveCommands = function receiveCommands (commands) {
-    this.dispatch.receiveCommands(commands);
+    this._globalDispatch.receiveCommands(commands);
     return this;
+};
+
+Clock.prototype.getGlobalDispatch = function getGlobalDispatch () {
+    return this._globalDispatch;
 };
 
 module.exports = new Clock();
