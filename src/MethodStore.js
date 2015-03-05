40fb936e58d7f40776b03e8b56e9fefdf2eb6a14
Alexander Gugel <alexander.gugel@gmail.com>
2015-03-05 14:14:01 -0800

    doc: Document basic CallbackStore functionality

--- src/MethodStore.js
+++ src/MethodStore.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function MethodStore () {
-    this.events = {};
+    this._events = {};
 }
 
 MethodStore.prototype.on = function on (key, cbclass, cbname) {
-    var events = this.events[key];
+    var events = this._events[key];
     if (!events) events = [];
     events.push(cbclass, cbname);
     return this;
 }
 
 MethodStore.prototype.off = function off (key, cbclass) {
-    var events = this.events[key];
+    var events = this._events[key];
     if (events) {
         var index = events.indexOf(cbclass);
         if (index > -1) events.splice(index, 2);
@@ -21,7 +21,7 @@ MethodStore.prototype.off = function off (key, cbclass) {
 }
 
 MethodStore.prototype.trigger = function trigger (key, payload) {
-    var events = this.events[key];
+    var events = this._events[key];
     if (events) {
         var i = 0;
         var len = events.length;
