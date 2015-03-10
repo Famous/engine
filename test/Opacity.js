7aa27af8b0e43f4faf13826931bcfbebbb301e0a
Alexander Gugel <alexander.gugel@gmail.com>
2015-03-09 18:57:30 -0700

    test: Test Opacity

--- test/Opacity.js
+++ test/Opacity.js
@@ -5,25 +5,50 @@ var Opacity = require('../src/Opacity');
 
 test('Opacity', function(t) {
     t.test('constructor', function(t) {
-        t.plan(1);
         t.equal(typeof Opacity, 'function', 'Opacity should be a function');
+
+        var opacity = new Opacity();
+        t.equal(opacity.isActive, false);
+        t.equal(opacity.dirty, false);
+
+        t.end();
     });
 
     t.test('set method', function(t) {
-        t.plan(1);
-        var align = new Opacity();
-        t.equal(typeof align.set, 'function', 'align.set should be a function');
+        var opacity = new Opacity();
+        t.equal(typeof opacity.set, 'function', 'opacity.set should be a function');
+
+        t.equal(opacity.set(0.1), opacity, 'opacity.set should be chainable');
+        t.equal(opacity.value, 0.1);
+        t.equal(opacity.dirty, true, 'opacity.set should dirty the opacity');
+
+        opacity.clean();
+        t.equal(opacity.dirty, false);
+
+        opacity.set(0.9);
+        t.equal(opacity.value, 0.9);
+        t.equal(opacity.dirty, true);
+
+        t.end();
     });
 
     t.test('setDirty method', function(t) {
-        t.plan(1);
-        var align = new Opacity();
-        t.equal(typeof align.setDirty, 'function', 'align.setDirty should be a function');
+        var opacity = new Opacity();
+        t.equal(typeof opacity.setDirty, 'function', 'opacity.setDirty should be a function');
+
+        t.equal(opacity.setDirty(), opacity, 'opacity.setDirty should be chainable');
+        t.equal(opacity.dirty, true);
+        opacity.clean();
+        t.equal(opacity.dirty, false);
+
+        t.end();
     });
 
     t.test('clean method', function(t) {
-        t.plan(1);
-        var align = new Opacity();
-        t.equal(typeof align.clean, 'function', 'align.clean should be a function');
+        var opacity = new Opacity();
+        t.equal(typeof opacity.clean, 'function', 'opacity.clean should be a function');
+
+        t.equal(opacity.clean(), opacity, 'opacity.clean should be chainable');
+        t.end();
     });
 });
