bc3973103d8439828792304a1ee191b08b56c125
Farhad Ghayour <f@famo.us>
2015-03-11 16:01:38 -0700

    Clean: Update baseColor to Material

--- src/chunks/applyLight.glsl
+++ src/chunks/applyLight.glsl
@@ -1,8 +1,8 @@
 #pragma glslify: lambertianLight = require(./lambertianLight)
 #pragma glslify: phongLight = require(./phongLight)
 
-vec3 applyLight(in vec3 baseColor) {
-    return (glossiness > 0.0) ? phongLight(baseColor) : lambertianLight(baseColor);
+vec3 applyLight(in vec3 material) {
+    return (glossiness > 0.0) ? phongLight(material) : lambertianLight(material);
 }
 
 #pragma glslify: export(applyLight)
