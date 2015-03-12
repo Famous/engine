1be71c274ed4c0e825828d8d6d9115722cfd212e
redwoodfavorite <joseph.michael.sample@gmail.com>
2015-03-11 20:05:23 -0700

    feat: adding TextureRegistry

---
+++ src/TextureRegistry.js
@@ -0,0 +1,20 @@
+var TextureRegistry = {
+	registry: {},
+
+	textureIds: 0,
+
+	register: function register(accessor, data) {
+		this.registry[accessor] = { id: this.textureIds++, data: data }
+	},
+
+	get: function get(accessor) {
+		if (!this.registry[accessor]) {
+			throw 'Texture "' + accessor + '" not found!';
+		}
+		else {
+			return this.registry[accessor];
+		}
+	}
+}
+
+module.exports = TextureRegistry;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-    Material: require('./Material')
+    Material: require('./Material'),
+    TextureRegistry: require('./TextureRegistry')
 };
