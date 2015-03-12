d60c6dd861ec3da76e7f02d0675188e6b127b1ec
Farhad Ghayour <f@famo.us>
2015-03-12 14:03:12 -0700

    Feat: Ambient and PointLight extending Light class

--- src/lights/AmbientLight.js
+++ src/lights/AmbientLight.js
@@ -3,67 +3,26 @@
 /**
  * Module dependencies
  */
-var Color = require('famous-utilities').Color;
+var Light = require('./Light');
 
 
 /**
  * AmbientLight Component
  */
 var AmbientLight = function AmbientLight(dispatch) {
-    this.dispatch = dispatch;
-    this.queue = [];
-    this._color = new Color();
-    this._id = dispatch.addComponent(this);
+    Light.call(this, dispatch);
+
+    this.commands = {
+        color: 'GL_AMBIENT_LIGHT'
+    };
 };
 
 AmbientLight.toString = function toString() {
     return 'AmbientLight';
 };
 
-AmbientLight.prototype.setColor = function setColor() {
-    this.dispatch.dirtyComponent(this._id);
-    var values = Array.prototype.concat.apply([], arguments);
-    if (values[0] instanceof Color) {
-        this._color = materialExpression[0];
-    }
-    else {
-        this._color.set(values);
-    }
-    this.queue.push('GL_AMBIENT_LIGHT');
-    var color = this._color.getNormalizedRGB();
-    this.queue.push(color[0]);
-    this.queue.push(color[1]);
-    this.queue.push(color[2]);
-    return this;
-};
-
-AmbientLight.prototype.getColor = function getColor(option) {
-    return this._color.getColor(option);
-};
-
-AmbientLight.prototype.clean = function clean() {
-    var path = this.dispatch.getRenderPath();
-
-    this.dispatch
-        .sendDrawCommand('WITH')
-        .sendDrawCommand(path);
-
-    var i = this.queue.length;
-    while (i--) {
-        this.dispatch.sendDrawCommand(this.queue.shift());
-    }
-
-    if (this._color.isActive()) {
-        this.dispatch.sendDrawCommand('GL_AMBIENT_LIGHT');
-        var color = this._color.getNormalizedRGB();
-        this.dispatch.sendDrawCommand(color[0]);
-        this.dispatch.sendDrawCommand(color[1]);
-        this.dispatch.sendDrawCommand(color[2]);
-        return true;
-    }
-
-    return false;
-};
+AmbientLight.prototype = Object.create(Light.prototype);
+AmbientLight.prototype.constructor = AmbientLight;
 
 
 /**
---
+++ src/lights/Light.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var Color = require('famous-utilities').Color;
+var helpers = require('famous-utilities').helpers;
+
+
+/**
+ * Light Component
+ */
+var Light = function Light(dispatch) {
+    this.dispatch = dispatch;
+    this._id = dispatch.addComponent(this);
+    this.queue = [];
+    this._color = new Color();
+    this.commands = { color: '' };
+};
+
+Light.toString = function toString() {
+    return 'Light';
+};
+
+Light.prototype.setColor = function setColor() {
+    this.dispatch.dirtyComponent(this._id);
+    var values = helpers.flattenArguments(arguments);
+
+    if (values[0] instanceof Color) {
+        this._color = materialExpression[0];
+    }
+
+    this._color.set(values);
+    this.queue.push(this.commands.color);
+    var color = this._color.getNormalizedRGB();
+    this.queue.push(color[0]);
+    this.queue.push(color[1]);
+    this.queue.push(color[2]);
+    return this;
+};
+
+Light.prototype.getColor = function getColor(option) {
+    return this._color.getColor(option);
+};
+
+Light.prototype.clean = function clean() {
+    var path = this.dispatch.getRenderPath();
+
+    this.dispatch
+        .sendDrawCommand('WITH')
+        .sendDrawCommand(path);
+
+    var i = this.queue.length;
+    while (i--) {
+        this.dispatch.sendDrawCommand(this.queue.shift());
+    }
+
+    if (this._color.isActive()) {
+        this.dispatch.sendDrawCommand(this.commands.color);
+        var color = this._color.getNormalizedRGB();
+        this.dispatch.sendDrawCommand(color[0]);
+        this.dispatch.sendDrawCommand(color[1]);
+        this.dispatch.sendDrawCommand(color[2]);
+        return true;
+    }
+
+    return false;
+};
+
+
+/**
+ * Expose
+ */
+module.exports = Light;
