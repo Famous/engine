40d85de4dd6d514dac6aefa8f4168a707d5096da
Farhad Ghayour <f@famo.us>
2015-03-12 22:47:21 -0700

    Clean: Updated lightdirection across fragment shader

--- src/chunks/phongLight.glsl
+++ src/chunks/phongLight.glsl
@@ -1,6 +1,5 @@
-vec3 phongLight(in vec3 material, in vec3 lightDir, in vec3 eyeVector) {
+vec3 phongLight(in vec3 material, in vec3 lightDirection, in vec3 eyeVector) {
     vec3 normal = normalize(v_Normal);
-    vec3 lightDirection = normalize(lightDir);
     float lambertTerm = dot(normal, -lightDirection);
     vec3 Id = vec3(0.0, 0.0, 0.0);
     vec3 Is = vec3(0.0, 0.0, 0.0);
