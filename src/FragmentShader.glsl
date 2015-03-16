04539df205ca38dda5b46a1d97c1d35bc8e2ad8a
Farhad Ghayour <f@famo.us>
2015-03-15 21:16:02 -0700

    Fix: Update ambient to account for flat shading

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -4,12 +4,13 @@
 void main() {
     vec3 material = baseColor.r >= 0.0 ? baseColor : applyMaterial(baseColor);
 
-    bool lightsEnabled = u_FlatShading < 1.0 && (u_LightColor.r > 0.0 || u_LightColor.g > 0.0 || u_LightColor.b > 0.0);
-    vec3 color = lightsEnabled ? applyLight(material) : material;
+    bool flatShading = u_FlatShading > 0.0;
+    bool lights = length(u_LightColor) > 0.0;
+    vec3 color = lights && !flatShading ? applyLight(material) : material;
 
-    bool ambianceEnabled = u_AmbientLight.r > 0.0 || u_AmbientLight.g > 0.0 || u_AmbientLight.b > 0.0;
+    bool ambianceEnabled = length(u_AmbientLight) > 0.0;
     vec3 ambience = u_AmbientLight * material;
-    vec3 finalColor = ambianceEnabled ? ambience + color : color;
+    vec3 finalColor = ambianceEnabled && !flatShading ? ambience + color : color;
 
     gl_FragColor = vec4(finalColor, opacity);
 }
