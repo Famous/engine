1aa891185c360b43cd06c9d17ca112ca6f2359f9
Ben Rowles <rowlesben@gmail.com>
2015-03-15 23:01:33 -0700

    fix: loop variable fix

--- src/UIEventHandler.js
+++ src/UIEventHandler.js
@@ -7,10 +7,10 @@ function UIEventHandler (dispatch, events) {
     var renderables = dispatch.getRenderables();
     for (var i = 0, len = renderables.length; i < len; i++)
         for (var j = 0, len2 = events.length; j < len2; j++) {
-            var eventName = events[i].event;
-            var methods = events[i].methods;
-            var properties = events[i].properties;
-            var callback = events[i].callback;
+            var eventName = events[j].event;
+            var methods = events[j].methods;
+            var properties = events[j].properties;
+            var callback = events[j].callback;
             this._events.on(eventName, callback);
             if (renderables[i].on) renderables[i].on(eventName, methods, properties);
             dispatch.registerTargetedEvent(eventName, this.trigger.bind(this, eventName));
