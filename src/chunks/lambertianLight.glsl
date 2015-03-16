9cf34702fc8c1226f53dee785ac40041a9c1c36f
Farhad Ghayour <f@famo.us>
2015-03-16 11:32:11 -0700

    Clean: Updating lighting model into single module

--- src/chunks/lambertianLight.glsl
+++
@@ -1,7 +0,0 @@
-vec3 lambertianLight(in vec3 material, in vec3 lightDirection) {
-    vec3 normal = normalize(v_Normal);
-    float nDotL = max(dot(-lightDirection, normal), 0.0);
-    return u_LightColor * material * nDotL;
-}
-
-#pragma glslify: export(lambertianLight)
