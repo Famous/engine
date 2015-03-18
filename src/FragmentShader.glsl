f747aa013fbe6f9d88f702af0f2769aef10ee81d
Farhad Ghayour <f@famo.us>
2015-03-17 22:46:52 -0700

    Feat: Add multiple lights support

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -4,8 +4,11 @@
 void main() {
     vec3 material = baseColor.r >= 0.0 ? baseColor : applyMaterial(baseColor);
 
-    bool lightsEnabled = (u_FlatShading == 0.0) &&
-                         (length(u_LightColor + u_AmbientLight) > 0.0);
+    /**
+     * Apply lights only if flat shading is false
+     * and at least one light is added to the scene
+     */
+    bool lightsEnabled = (u_FlatShading == 0.0) && (u_NumLights > 0.0);
     vec3 color = lightsEnabled ? applyLight(material) : material;
 
     gl_FragColor = vec4(color, opacity);
