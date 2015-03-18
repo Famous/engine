8b207a38adbfff90826b017c577046300a892102
Farhad Ghayour <f@famo.us>
2015-03-17 17:12:58 -0700

    Clean: Update lighting reflection model

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -4,13 +4,9 @@
 void main() {
     vec3 material = baseColor.r >= 0.0 ? baseColor : applyMaterial(baseColor);
 
-    bool flatShading = u_FlatShading > 0.0;
-    bool lights = length(u_LightColor) > 0.0;
-    vec3 color = lights && !flatShading ? applyLight(material) : material;
+    bool lightsEnabled = (u_FlatShading == 0.0) &&
+                         (length(u_LightColor + u_AmbientLight) > 0.0);
+    vec3 color = lightsEnabled ? applyLight(material) : material;
 
-    bool ambience = length(u_AmbientLight) > 0.0;
-    vec3 ambientLight = u_AmbientLight * material;
-    vec3 finalColor = ambience && !flatShading ? ambientLight + color : color;
-
-    gl_FragColor = vec4(finalColor, opacity);
+    gl_FragColor = vec4(color, opacity);
 }
--- src/chunks/applyLight.glsl
+++ src/chunks/applyLight.glsl
@@ -1,9 +1,27 @@
-#pragma glslify: phongLight = require(./phongLight)
-
 vec3 applyLight(in vec3 material) {
+    vec3 finalColor;
+    vec3 ambientLight = u_AmbientLight * material;
     vec3 lightDirection = normalize(u_LightPosition - v_Position);
     vec3 eyeVector = -normalize(vec3(v_Position));
-    return phongLight(material, lightDirection, eyeVector);
+    vec3 normal = normalize(v_Normal);
+    float lambertianTerm = dot(lightDirection, normal);
+    vec3 diffuse = vec3(0.0, 0.0, 0.0);
+    vec3 specular = vec3(0.0, 0.0, 0.0);
+
+    if (lambertianTerm > 0.0 && glossiness > 0.0) {
+        diffuse = material * lambertianTerm;
+        vec3 E = normalize(eyeVector);
+        vec3 R = reflect(lightDirection, normal);
+        float specularWeight = pow(max(dot(R, E), 0.0), glossiness);
+        specular = u_LightColor * specularWeight;
+        finalColor = diffuse + specular;
+    }
+    else {
+        lambertianTerm = max(lambertianTerm, 0.0);
+        finalColor = u_LightColor * material * lambertianTerm;
+    }
+
+    return ambientLight + finalColor;
 }
 
 #pragma glslify: export(applyLight)
