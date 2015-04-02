0d7f76a3a573735e49629b15ddcff049d36f433c
Farhad Ghayour <f@famo.us>
2015-04-02 14:31:39 -0700

    Clean: Update debug markup and styling

--- src/Debug.js
+++ src/Debug.js
@@ -40,21 +40,21 @@ function _augmentFunction(func, callback) {
  * Parses errors and failed source code from shaders in order
  * to build displayable error blocks.
  *
+ * Inspired by Jaume Sanchez Elias
+ *
  * @param {String} Errors
  * @param {String} Source
  */
 function _processErrors(errors, source) {
 
-    var css = '#shaderReport{ box-sizing: border-box; position: absolute; left: 0; top: 0; \
-        right: 0; font-family: monaco, monospace; font-size: 12px; z-index: 1000; \
-        background-color: #b70000; color: #fff; white-space: normal; \
-        text-shadow: 0 -1px 0 rgba(0,0,0,.6); line-height: 1.2em; list-style-type: none; \
-        padding: 0; margin: 0; max-height: 300px; overflow: auto; } \
-        #shaderReport li{ padding: 20px 10px; border-top: 1px solid rgba( 255, 255, 255, .2 ); \
-        border-bottom: 1px solid rgba( 0, 0, 0, .2 ) } \
-        #shaderReport li p{ padding: 0; margin: 0 } \
-        #shaderReport li:nth-child(odd){ background-color: #9D0B0B }\
-        #shaderReport li p:first-child{ color: #fff }';
+    var css = `
+        body,html{background:#e8e8e8;font-family:monaco,monospace;font-size:14px;line-height:1.7em}
+        #shaderReport{left:0;top:0;right:0;box-sizing:border-box;position:absolute;z-index:1000;
+            color:#888;padding:0;white-space:normal;list-style-type:none;margin:50px auto;max-width:1200px}
+        #shaderReport li{background-color:#fff;margin:10px 0;box-shadow:0 1px 2px rgba(0,0,0,.15);
+            padding:20px 30px;border-radius:2px;border-left:20px solid red}#shaderReport li p{padding:0;margin:0}
+        #shaderReport li:nth-child(even){background-color:#f8f8f8}#shaderReport li p:first-child{font-weight:700;color:#222}
+    `;
 
     var el = document.createElement('style');
     document.getElementsByTagName('head')[0].appendChild(el);
@@ -69,9 +69,7 @@ function _processErrors(errors, source) {
 
     var m;
     while ((m = re.exec(errors)) != null) {
-        if (m.index === re.lastIndex) {
-            re.lastIndex++;
-        }
+        if (m.index === re.lastIndex) re.lastIndex++;
         var li = document.createElement('li');
         var code = '<p>ERROR "<b>' + m[2] + '</b>" in line ' + m[1] + '</p>'
         code += '<p>' + lines[m[1] - 1].replace(/^[ \t]+/g, '') + '</p>';
