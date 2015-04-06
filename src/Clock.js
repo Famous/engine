e5c696c2363241096e74951585d9ea0a8d3ed8bf
Alexander Gugel <alexander.gugel@gmail.com>
2015-04-06 12:26:51 -0700

    fix: Fix race condition in Clock

--- src/Clock.js
+++ src/Clock.js
@@ -28,14 +28,9 @@ function Clock () {
 Clock.prototype.step = function step (time) {
     this._frame++;
     this._time = time;
-    for (var i = 0; i < this._timerQueue.length; i += 5) {
-        if (this._time > this._timerQueue[i+1] + this._timerQueue[i+2]) {
-            this._timerQueue[i].apply(this, this._timerQueue[i+3]);
-            if (!this._timerQueue[i+4]) {
-                this._timerQueue.splice(i, i+5);
-            } else {
-                this._timerQueue[i+1] = this._time;
-            }
+    for (var i = 0; i < this._timerQueue.length; i++) {
+        if (this._timerQueue[i](this._time)) {
+            this._timerQueue.splice(i, 1);
         }
     }
     return this;
@@ -87,12 +82,22 @@ Clock.prototype.getFrame = function getFrame () {
  * @param {Function} callback function to be run after a specified duration
  * @param {Number} delay milliseconds from now to execute the function
  *
- * @return {Function} decorated passed in callback function
+ * @return {Function} timer function used for Clock#clearTimer
  */
 Clock.prototype.setTimeout = function (callback, delay) {
     var params = Array.prototype.slice.call(arguments, 2);
-    return this._timerQueue.push(callback, this._time, delay, params, false);
+    var startedAt = this._time;
+    var timer = function(time) {
+        if (time - startedAt >= delay) {
+            callback.apply(null, params);
+            return true;
+        }
+        return false;
+    };
+    this._timerQueue.push(timer);
+    return timer;
 };
+
 
 /**
  * Wraps a function to be invoked after a certain amount of time.
@@ -104,40 +109,37 @@ Clock.prototype.setTimeout = function (callback, delay) {
  * @param {Function} callback function to be run after a specified duration
  * @param {Number} duration interval to execute function in milliseconds
  *
- * @return {Function} decorated passed in callback function
+ * @return {Function} timer function used for Clock#clearTimer
  */
 Clock.prototype.setInterval = function setInterval(callback, delay) {
     var params = Array.prototype.slice.call(arguments, 2);
-    return this._timerQueue.push(callback, this._time, delay, params, true);
+    var startedAt = this._time;
+    var timer = function(time) {
+        if (time - startedAt >= delay) {
+            callback.apply(null, params);
+            startedAt = time;
+        }
+        return false;
+    };
+    this._timerQueue.push(timer);
+    return timer;
 };
 
 /**
- * Removes previously via `Clock#setInterval` registered callback function.
+ * Removes previously via `Clock#setTimeout` or `Clock#setInterval`
+ * registered callback function
  *
- * @method clearInterval
+ * @method clearTimer
  * @chainable
  * 
- * @param  {Number} intervalID  by `Clock#setInterval` returned intervalID
+ * @param  {Function} callback  previously by `Clock#setTimeout` or
+ *                              `Clock#setInterval` returned callback function
  * @return {Clock}              this
  */
-Clock.prototype.clearInterval = function clearInterval(intervalID) {
-    if (this._timerQueue[intervalID-1] === true) {
-        this._timerQueue.splice(intervalID-5, intervalID);
-    }
-};
-
-/**
- * Removes previously via `Clock#setTimeout` registered callback function.
- *
- * @method clearTimeout
- * @chainable
- * 
- * @param  {Number} timeoutID   by `Clock#setTimeout` returned timeoutID
- * @return {Clock}              this
- */
-Clock.prototype.clearTimeout = function clearTimeout(timeoutID) {
-    if (this._timerQueue[timeoutID-1] === false) {
-        this._timerQueue.splice(timeoutID-5, timeoutID);
+Clock.prototype.clearTimer = function (timer) {
+    var index = this._timerQueue.indexOf(timer);
+    if (index !== -1) {
+        this._timerQueue.splice(index, 1);
     }
     return this;
 };
