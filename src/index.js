7957fbfc6d904c4a5e917247a2ea06d21af32805
DnMllr <me@dnmllr.io>
2015-04-07 15:41:19 -0700

    feature: removed depricated files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 'use strict';
 
 module.exports = {
-    // Align: require('./Align'),
-    // ComponentStore: require('./ComponentStore'),
-    // GlobalDispatch: require('./GlobalDispatch'),
+    Clock: require('./Clock'),
+    Event: require('./Event'),
     Context: require('./Context'),
     Famous: require('./Famous'),
     Dispatcher: require('./Dispatcher'),
-    // Layer: require('./Layer'),
-    // Clock: require('./Clock'),
-    // LocalDispatch: require('./LocalDispatch'),
-    // MountPoint: require('./MountPoint'),
     Node: require('./Node'),
-    Size: require('./Size')
-    // Opacity: require('./Opacity'),
-    // Origin: require('./Origin'),
-    // RenderContext: require('./RenderContext'),
-    // RenderProxy: require('./RenderProxy'),
-    // Size: require('./Size'),
-    // Transform: require('./Transform')
+    Size: require('./Size'),
+    Transform: require('./Transform')
 };
