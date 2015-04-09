9b5128a19353107826b44dc3f25ed6ae2260907a
DnMllr <me@dnmllr.io>
2015-04-09 11:31:54 -0700

    fix: added comments for Context

--- src/Context.js
+++ src/Context.js
@@ -4,6 +4,20 @@ var Dispatch = require('./Dispatcher');
 var Node = require('./Node');
 var Size = require('./Size');
 
+/**
+ * Context is the bottom of the scene graph. It is it's own
+ * parent and provides the global updater to the scene graph.
+ *
+ * @class Context
+ * @constructor
+ *
+ * @param {String} selector a string which is a dom selector
+ *                 signifying which dom element the context
+ *                 should be set upon
+ * @param {Famous} a class which conforms to Famous' interface
+ *                 it needs to be able to send methods to
+ *                 the renderers and update nodes in the scene graph
+ */
 function Context (selector, famous) {
     Node.call(this);
     this._famous = famous;
@@ -16,26 +30,46 @@ function Context (selector, famous) {
     this.show();
 }
 
+// Context inherits from node
 Context.prototype = Object.create(Node.prototype);
 Context.prototype.constructor = Context;
 
+/**
+ * Context getUpdater function returns the passed in updater
+ *
+ * @return {Famous} the updater for this Context
+ */
 Context.prototype.getUpdater = function getUpdater () {
     return this._famous;
 };
 
+/**
+ * Returns the selector that the context was instantiated with
+ *
+ * @return {String} dom selector
+ */
 Context.prototype.getSelector = function getSelector () {
     return this._selector;
 };
 
+/**
+ * Returns the dispatcher of the context. Used to send events
+ * to the nodes in the scene graph.
+ *
+ * @return {Dispatcher} the Context's Dispatcher
+ */
 Context.prototype.getDispatch = function getDispatch () {
     return this._dispatch;
 };
 
-Context.prototype._receiveContextSize = function _receiveContextSize (size) {
-    this.setSizeMode(Size.ABSOLUTE, Size.ABSOLUTE, Size.ABSOLUTE);
-    this.setAbsoluteSize(size[0], size[1], size[2]);
-};
-
+/**
+ * Receives an event. If the event is 'CONTEXT_RESIZE' it sets the size of the scene
+ * graph to the payload, which must be an array of numbers of at least
+ * length three representing the pixel size in 3 dimensions.
+ *
+ * @param {String} event
+ * @param {*} payload
+ */
 Context.prototype.onReceive = function onReceive (event, payload) {
     if (event === 'CONTEXT_RESIZE') {
         this.setSizeMode(Size.ABSOLUTE, Size.ABSOLUTE, Size.ABSOLUTE);
