174a1a4d98db52ac1472f4268195a538e6626800
Farhad Ghayour <f@famo.us>
2015-04-27 16:03:19 -0700

    Feat: Update lighting model to a faster reflection model

--- src/chunks/applyLight.glsl
+++ src/chunks/applyLight.glsl
@@ -1,42 +1,36 @@
 /**
  * Calculates the intensity of light on a surface.
- * 
- * @method transpose
+ *
+ * @method applyLight
  * @private
  *
- *
  */
-
 vec3 applyLight(in vec3 material) {
     int numLights = int(u_NumLights);
-    float lambertianTerm;
-    vec3 finalColor = vec3(0.0);
+    vec3 ambientColor = u_AmbientLight * material;
     vec3 normal = normalize(v_Normal);
-    vec3 ambientLight = u_AmbientLight * material;
-    vec3 eyeVector = vec3(-v_Position);
-    vec3 diffuse, specular, lightDirection;
+    vec3 center = resolution * 0.5;
+    vec3 eyeVector = normalize(center - v_Position);
+    vec3 specular = vec3(0.0);
+    vec3 finalColor = vec3(0.0);
 
     for(int i = 0; i < 4; i++) {
         if (i >= numLights) break;
-        diffuse = vec3(0.0, 0.0, 0.0);
-        specular = vec3(0.0, 0.0, 0.0);
-        lightDirection = normalize(u_LightPosition[i].xyz - v_Position);
-        lambertianTerm = dot(lightDirection, normal);
-        if (lambertianTerm > 0.0 && glossiness > 0.0) {
-            diffuse = material * lambertianTerm;
-            vec3 E = normalize(eyeVector);
-            vec3 R = reflect(lightDirection, normal);
-            float specularWeight = pow(max(dot(R, E), 0.0), glossiness);
-            specular = u_LightColor[i].rgb * specularWeight;
-            finalColor += diffuse + specular;
+        vec3 lightDirection = normalize(u_LightPosition[i].xyz - v_Position);
+        float lambertian = max(dot(lightDirection, normal), 0.0);
+        vec3 diffuse = u_LightColor[i].rgb * material;
+
+        if (lambertian > 0.0) {
+            vec3 halfVector = normalize(lightDirection + eyeVector);
+            float specularAngle = max(dot(halfVector, normal), 0.0);
+            specular = u_LightColor[i].rgb * pow(specularAngle, glossiness * 10.0);
+            if (glossiness > 0.0) finalColor += specular;
         }
-        else {
-            lambertianTerm = max(lambertianTerm, 0.0);
-            finalColor += u_LightColor[i].rgb * material * lambertianTerm;
-        }
+
+        finalColor += lambertian * diffuse;
     }
 
-    return ambientLight + finalColor;
+    return ambientColor + finalColor;
 }
 
 #pragma glslify: export(applyLight)
