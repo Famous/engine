f069b192beae0b676fc19a19606275709ee72562
Farhad Ghayour <f@famo.us>
2015-04-27 18:09:11 -0700

    Feat: Add glossiness to lighting equation

--- src/chunks/applyLight.glsl
+++ src/chunks/applyLight.glsl
@@ -9,28 +9,27 @@ vec3 applyLight(in vec3 material) {
     int numLights = int(u_NumLights);
     vec3 ambientColor = u_AmbientLight * material;
     vec3 normal = normalize(v_Normal);
-    vec3 center = resolution * 0.5;
-    vec3 eyeVector = normalize(center - v_Position);
+    vec3 eyeVector = normalize(v_EyeVector);
     vec3 specular = vec3(0.0);
-    vec3 finalColor = vec3(0.0);
+    vec3 diffuse = vec3(0.0);
 
     for(int i = 0; i < 4; i++) {
         if (i >= numLights) break;
         vec3 lightDirection = normalize(u_LightPosition[i].xyz - v_Position);
         float lambertian = max(dot(lightDirection, normal), 0.0);
-        vec3 diffuse = u_LightColor[i].rgb * material;
 
         if (lambertian > 0.0) {
-            vec3 halfVector = normalize(lightDirection + eyeVector);
-            float specularAngle = max(dot(halfVector, normal), 0.0);
-            specular = u_LightColor[i].rgb * pow(specularAngle, glossiness * 10.0);
-            if (glossiness > 0.0) finalColor += specular;
+            diffuse += u_LightColor[i].rgb * material * lambertian;
         }
 
-        finalColor += lambertian * diffuse;
+        if (glossiness > 0.0) {
+            vec3 halfVector = normalize(lightDirection + eyeVector);
+            float specular = pow(max(dot(halfVector, normal), 0.0), glossiness);
+            diffuse += u_LightColor[i].rgb * specular;
+        }
     }
 
-    return ambientColor + finalColor;
+    return ambientColor + diffuse;
 }
 
 #pragma glslify: export(applyLight)
