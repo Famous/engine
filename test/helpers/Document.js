3512486638757cb42f756d534633de991e8476ef
redwoodfavorite <joseph.michael.sample@gmail.com>
2015-04-30 12:10:41 -0700

    chore: removing unused helper files

--- test/helpers/Document.js
+++
@@ -1,9 +0,0 @@
-var CanvasElement = require('./Canvas');
-
-var document = {
-	createElement: function(type) {
-		if (type === 'canvas') return new CanvasElement();
-	}
-}
-
-module.exports = document;
