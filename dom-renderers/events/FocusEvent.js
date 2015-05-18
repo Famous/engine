8dd9318848574be957fa2f40003ff5476954eb48
Alexander Gugel <alexander.gugel@gmail.com>
2015-05-18 13:23:23 -0700

    doc: Document eventing system

--- dom-renderers/events/FocusEvent.js
+++ dom-renderers/events/FocusEvent.js
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Famous Industries Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,14 @@
 
 var UIEvent = require('./UIEvent');
 
+/**
+ * See [UI Events (formerly DOM Level 3 Events)](http://www.w3.org/TR/2015/WD-uievents-20150428/#events-focusevent).
+ *
+ * @class    FocusEvent
+ * @augments UIEvent
+ *
+ * @param  {Event} ev   The native DOM event.
+ */
 function FocusEvent(ev) {
     // [Constructor(DOMString typeArg, optional FocusEventInit focusEventInitDict)]
     // interface FocusEvent : UIEvent {
--- dom-renderers/events/UIEvent.js
+++ dom-renderers/events/UIEvent.js
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Famous Industries Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,21 +21,33 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+
 /*jshint -W079 */
 
 'use strict';
 
 var Event = require('./Event');
 
+/**
+ * See [UI Events (formerly DOM Level 3 Events)](http://www.w3.org/TR/2015/WD-uievents-20150428).
+ *
+ * @class               UIEvent
+ * @augments            Event
+ *
+ * @param  {Event} ev   The native DOM event.
+ */
 function UIEvent(ev) {
     // [Constructor(DOMString type, optional UIEventInit eventInitDict)]
     // interface UIEvent : Event {
     //     readonly    attribute Window? view;
     //     readonly    attribute long    detail;
     // };
-
     Event.call(this, ev);
+
+    /**
+     * @name UIEvent#detail
+     * @type Number
+     */
     this.detail = ev.detail;
 }
 
