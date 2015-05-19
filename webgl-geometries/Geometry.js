a2937bf149e2888569a97cd2067f233d918b9c35
Farhad Ghayour <f@famo.us>
2015-05-19 16:12:12 -0700

    Clean: Update docs for Geometry

--- webgl-geometries/Geometry.js
+++ webgl-geometries/Geometry.js
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Famous Industries Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,13 +27,13 @@
 var GeometryIds = 0;
 
 /**
- * Geometry is a component that defines the data that should
- * be drawn to the webGL canvas. Manages vertex data and attributes.
+ * Geometry is a component that defines and manages data
+ * (vertex data and attributes) that is used to draw to WebGL.
  *
  * @class Geometry
  * @constructor
- * 
- * @param {Object} options Instantiation options.
+ *
+ * @param {Object} options instantiation options
  */
 function Geometry(options) {
     this.options = options || {};
