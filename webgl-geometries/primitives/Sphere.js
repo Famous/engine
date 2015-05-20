cd1ae5a46d175628413451427f99a755cccc82f2
Farhad Ghayour <f@famo.us>
2015-05-20 16:06:17 -0700

    Chore: Docs for webgl-geometries

--- webgl-geometries/primitives/Sphere.js
+++ webgl-geometries/primitives/Sphere.js
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Famous Industries Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,13 +36,13 @@ var GeometryHelper = require('../GeometryHelper');
  *
  * @param {Object} options Parameters that alter the
  * vertex buffers of the generated geometry.
- * 
+ *
  * @return {Object} constructed geometry
  */
 function ParametricSphere (options) {
     options = options || {};
     var detail = options.detail || 10;
-    var detailX = options.detailX || detail;    
+    var detailX = options.detailX || detail;
     var detailY = options.detailY || detail;
 
     var buffers = GeometryHelper.generateParametric(
@@ -68,7 +68,7 @@ function ParametricSphere (options) {
  * Function used in iterative construction of parametric primitive.
  *
  * @static
- * @method generator
+ * @method
  * @param {Number} u Longitudal progress from 0 to PI.
  * @param {Number} v Latitudal progress from 0 to PI.
  * @return {Array} x, y and z coordinates of geometry
