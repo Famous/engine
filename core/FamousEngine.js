b50a9062904c8499d8fd75e0a88946f356568910
Alexander Gugel <alexander.gugel@gmail.com>
2015-05-29 10:56:15 -0700

    fix: Fix initial glitch (Context solution)

--- core/FamousEngine.js
+++ core/FamousEngine.js
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Famous Industries Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -68,6 +68,7 @@ function FamousEngine() {
 
     this._clock = new Clock(); // a clock to keep track of time for the scene
                                // graph.
+
 
     this._channel = new Channel();
     this._channel.onMessage = function (message) {
@@ -317,7 +318,7 @@ FamousEngine.prototype.getContext = function getContext (selector) {
 };
 
 /**
- * returns the instance of clock within famous.
+ * Returns the instance of clock used by the FamousEngine.
  *
  * @method
  *
@@ -328,7 +329,7 @@ FamousEngine.prototype.getClock = function getClock () {
 };
 
 /**
- * queues a message to be transfered to the renderers.
+ * Enqueues a message to be transfered to the renderers.
  *
  * @method
  *
