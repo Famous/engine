de7131981432554e832f917dc9c3344d4df6b166
Farhad Ghayour <f@famo.us>
2015-06-01 12:07:03 -0700

    Clean: Update css injection to a cleaner implementation

--- renderers/inject-css.js
+++ renderers/inject-css.js
@@ -24,44 +24,44 @@
 
 'use strict';
 
-var css = '.famous-dom-renderer {' +
-    'width:100%;' +
-    'height:100%;' +
-    'transform-style:preserve-3d;' +
-    '-webkit-transform-style:preserve-3d;' +
-'}' +
+var css = ''
++'.famous-dom-renderer{'
+    +'width:100%;'
+    +'height:100%;'
+    +'transform-style:preserve-3d;'
+    +'-webkit-transform-style:preserve-3d;'
++'}'
 
-'.famous-dom-element {' +
-    '-webkit-transform-origin:0% 0%;' +
-    'transform-origin:0% 0%;' +
-    '-webkit-backface-visibility:visible;' +
-    'backface-visibility:visible;' +
-    '-webkit-transform-style:preserve-3d;' +
-    'transform-style:preserve-3d;' +
-    '-webkit-tap-highlight-color:transparent;' +
-    'pointer-events:auto;' +
-    'z-index:1;' +
-'}' +
++'.famous-dom-element{'
+    +'-webkit-transform-origin:0% 0%;'
+    +'transform-origin:0% 0%;'
+    +'-webkit-backface-visibility:visible;'
+    +'backface-visibility:visible;'
+    +'-webkit-transform-style:preserve-3d;'
+    +'transform-style:preserve-3d;'
+    +'-webkit-tap-highlight-color:transparent;'
+    +'pointer-events:auto;'
+    +'z-index:1;'
++'}'
 
-'.famous-dom-element-content,' +
-'.famous-dom-element {' +
-    'position:absolute;' +
-    'box-sizing:border-box;' +
-    '-moz-box-sizing:border-box;' +
-    '-webkit-box-sizing:border-box;' +
-'}' +
++'.famous-dom-element-content,'
++'.famous-dom-element{'
+    +'position:absolute;'
+    +'box-sizing:border-box;'
+    +'-moz-box-sizing:border-box;'
+    +'-webkit-box-sizing:border-box;'
++'}'
 
-'.famous-webgl-renderer {' +
-    '-webkit-transform: translateZ(1000000px);' +  /* TODO: Fix when Safari Fixes*/
-    'transform: translateZ(1000000px)' +
-    'pointer-events:none;' +
-    'position:absolute;' +
-    'z-index:1;' +
-    'top:0;' +
-    'left:0;' +
-    'width:100%;' +
-    'height:100%;' +
-'}';
++'.famous-webgl-renderer{'
+    +'-webkit-transform: translateZ(1000000px);'  /* TODO: Fix when Safari Fixes*/
+    +'transform: translateZ(1000000px)'
+    +'pointer-events:none;'
+    +'position:absolute;'
+    +'z-index:1;'
+    +'top:0;'
+    +'width:100%;'
+    +'height:100%;'
++'}';
 
 var INJECTED = typeof document === 'undefined';
 
