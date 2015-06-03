142e1deccf0d8b160294134973073a97f062e83a
Alexander Gugel <alexander.gugel@gmail.com>
2015-06-03 12:00:23 +0200

    refact: Clean up Context
    * Remove unused properties
    * Move canvas resize to WebGLRenderer (like in DOMRenderer)
    * Follow _-prefix convention for private properties
    * Instantiate DOMRenderer in _initDOMRenderer method
    
    The primary intent of those changes is to make the interface of
    DOMRenderer and WebGLRenderer more consistent.
    
    The above changes make it easier to make the DOMRenderer optional
    (similar to WebGLRenderer).

--- renderers/Context.js
+++ renderers/Context.js
@@ -46,27 +46,26 @@ var DOMRenderer = require('../dom-renderers/DOMRenderer');
  * context layer.
  * @param {Compositor} compositor Compositor reference to pass down to
  * WebGLRenderer.
- *
- * @return {undefined} undefined
  */
 function Context(selector, compositor) {
     this._compositor = compositor;
     this._rootEl = document.querySelector(selector);
 
+    if (this._rootEl === null) {
+        throw new Error(
+            'Failed to create Context: ' +
+            'No matches for "' + selector + '" found.'
+        );
+    }
+
     this._selector = selector;
 
-    // Create DOM element to be used as root for all famous DOM
-    // rendering and append element to the root element.
+    // Initializes the DOMRenderer.
+    // Every Context has at least a DOMRenderer for now.
+    this._initDOMRenderer();
 
-    this._domLayerEl = document.createElement('div');
-    this._domLayerEl.style.display = 'none';
-    this._rootEl.appendChild(this._domLayerEl);
-
-    // Instantiate renderers
-
-    this.DOMRenderer = new DOMRenderer(this._domLayerEl, selector, compositor);
-    this.WebGLRenderer = null;
-    this.canvas = null;
+    // WebGLRenderer will be instantiated when needed.
+    this._webGLRenderer = null;
 
     // State holders
 
@@ -79,28 +78,33 @@ function Context(selector, compositor) {
     };
 
     this._size = [];
-    this._children = {};
-    this._elementHash = {};
 
     this._meshTransform = [];
     this._meshSize = [0, 0, 0];
 
     this._initDOM = false;
+
+    this.updateSize();
 }
 
 /**
  * Queries DOMRenderer size and updates canvas size. Relays size information to
  * WebGLRenderer.
  *
+ * @method
+ *
  * @return {Context} this
  */
 Context.prototype.updateSize = function () {
-    var newSize = this.getRootSize();
-    this._compositor.sendResize(this._selector, newSize);
+    var width = this._rootEl.offsetWidth;
+    var height = this._rootEl.offsetHeight;
 
-    if (this.canvas && this.WebGLRenderer) {
-        this.WebGLRenderer.updateSize(newSize);
-    }
+    this._size[0] = width;
+    this._size[1] = height;
+    this._size[2] = (width > height) ? width : height;
+
+    this._compositor.sendResize(this._selector, this._size);
+    if (this._webGLRenderer) this._webGLRenderer.updateSize(this._size);
 
     return this;
 };
@@ -114,15 +118,55 @@ Context.prototype.updateSize = function () {
  * @return {undefined} undefined
  */
 Context.prototype.draw = function draw() {
-    this.DOMRenderer.draw(this._renderState);
-    if (this.WebGLRenderer) this.WebGLRenderer.draw(this._renderState);
+    this._domRenderer.draw(this._renderState);
+    if (this._webGLRenderer) this._webGLRenderer.draw(this._renderState);
 
     if (this._renderState.perspectiveDirty) this._renderState.perspectiveDirty = false;
     if (this._renderState.viewDirty) this._renderState.viewDirty = false;
 };
 
+
+Context.prototype._initDOMRenderer = function _initDOMRenderer() {
+    this._domRendererRootEl = document.createElement('div');
+    this._rootEl.appendChild(this._domRendererRootEl);
+    this._domRendererRootEl.style.display = 'none';
+
+    this._domRenderer = new DOMRenderer(
+        this._domRendererRootEl,
+        this._selector,
+        this._compositor
+    );
+};
+
 /**
- * Determines the root element's offset size.
+ * Initializes the WebGLRenderer and updates it initial size.
+ *
+ * The Initialization process consists of the following steps:
+ *
+ * 1. A new `<canvas>` element is being created and appended to the root element.
+ * 2. The WebGLRenderer is being instantiated.
+ * 3. The size of the WebGLRenderer is being updated.
+ *
+ * @method
+ *
+ * @return {undefined} undefined
+ */
+Context.prototype._initWebGLRenderer = function _initWebGLRenderer() {
+    this._webGLRendererRootEl = document.createElement('canvas');
+    this._rootEl.appendChild(this._webGLRendererRootEl);
+
+    this._webGLRenderer = new WebGLRenderer(
+        this._webGLRendererRootEl,
+        this._compositor
+    );
+
+    // Don't read offset width and height.
+    this._webGLRenderer.updateSize(this._size);
+};
+
+
+/**
+ * Gets the size of the parent element of the DOMRenderer for this context.
  *
  * @method
  *
@@ -131,24 +175,8 @@ Context.prototype.draw = function draw() {
 Context.prototype.getRootSize = function getRootSize() {
     return [
         this._rootEl.offsetWidth,
-        this._rootEl.offsetHeight,
+        this._rootEl.offsetHeight
     ];
-};
-
-/**
- * Handles initialization of WebGLRenderer when necessary, including creation
- * of the canvas element and instantiation of the renderer. Also updates size
- * to pass size information to the renderer.
- *
- * @method
- *
- * @return {undefined} undefined
- */
-Context.prototype.initWebGL = function initWebGL() {
-    this.canvas = document.createElement('canvas');
-    this._rootEl.appendChild(this.canvas);
-    this.WebGLRenderer = new WebGLRenderer(this.canvas, this._compositor);
-    this.updateSize();
 };
 
 
@@ -159,7 +187,7 @@ Context.prototype.initWebGL = function initWebGL() {
  */
 Context.prototype.checkInit = function checkInit () {
     if (this._initDOM) {
-        this._domLayerEl.style.display = 'block';
+        this._domRendererRootEl.style.display = 'block';
         this._initDOM = false;
     }
 };
@@ -181,94 +209,92 @@ Context.prototype.receive = function receive(path, commands, iterator) {
     var localIterator = iterator;
 
     var command = commands[++localIterator];
-    this.DOMRenderer.loadPath(path);
-    this.DOMRenderer.findTarget();
+    this._domRenderer.loadPath(path);
+    this._domRenderer.findTarget();
     while (command) {
-
         switch (command) {
             case 'INIT_DOM':
-                this.DOMRenderer.insertEl(commands[++localIterator]);
+                this._domRenderer.insertEl(commands[++localIterator]);
                 break;
 
             case 'DOM_RENDER_SIZE':
-                this.DOMRenderer.getSizeOf(commands[++localIterator]);
+                this._domRenderer.getSizeOf(commands[++localIterator]);
                 break;
 
             case 'CHANGE_TRANSFORM':
                 for (var i = 0 ; i < 16 ; i++) this._meshTransform[i] = commands[++localIterator];
 
-                this.DOMRenderer.setMatrix(this._meshTransform);
+                this._domRenderer.setMatrix(this._meshTransform);
 
-                if (this.WebGLRenderer)
-                    this.WebGLRenderer.setCutoutUniform(path, 'u_transform', this._meshTransform);
-
+                if (this._webGLRenderer)
+                    this._webGLRenderer.setCutoutUniform(path, 'u_transform', this._meshTransform);
                 break;
 
             case 'CHANGE_SIZE':
                 var width = commands[++localIterator];
                 var height = commands[++localIterator];
 
-                this.DOMRenderer.setSize(width, height);
-                if (this.WebGLRenderer) {
+                this._domRenderer.setSize(width, height);
+                if (this._webGLRenderer) {
                     this._meshSize[0] = width;
                     this._meshSize[1] = height;
-                    this.WebGLRenderer.setCutoutUniform(path, 'u_size', this._meshSize);
+                    this._webGLRenderer.setCutoutUniform(path, 'u_size', this._meshSize);
                 }
                 break;
 
             case 'CHANGE_PROPERTY':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.setProperty(commands[++localIterator], commands[++localIterator]);
+                if (this._webGLRenderer) this._webGLRenderer.getOrSetCutout(path);
+                this._domRenderer.setProperty(commands[++localIterator], commands[++localIterator]);
                 break;
 
             case 'CHANGE_CONTENT':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.setContent(commands[++localIterator]);
+                if (this._webGLRenderer) this._webGLRenderer.getOrSetCutout(path);
+                this._domRenderer.setContent(commands[++localIterator]);
                 break;
 
             case 'CHANGE_ATTRIBUTE':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.setAttribute(commands[++localIterator], commands[++localIterator]);
+                if (this._webGLRenderer) this._webGLRenderer.getOrSetCutout(path);
+                this._domRenderer.setAttribute(commands[++localIterator], commands[++localIterator]);
                 break;
 
             case 'ADD_CLASS':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.addClass(commands[++localIterator]);
+                if (this._webGLRenderer) this._webGLRenderer.getOrSetCutout(path);
+                this._domRenderer.addClass(commands[++localIterator]);
                 break;
 
             case 'REMOVE_CLASS':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.removeClass(commands[++localIterator]);
+                if (this._webGLRenderer) this._webGLRenderer.getOrSetCutout(path);
+                this._domRenderer.removeClass(commands[++localIterator]);
                 break;
 
             case 'SUBSCRIBE':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.subscribe(commands[++localIterator]);
+            if (this._webGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
+                this._domRenderer.subscribe(commands[++localIterator]);
                 break;
 
             case 'UNSUBSCRIBE':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.unsubscribe(commands[++localIterator]);
+            if (this._webGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
+                this._domRenderer.unsubscribe(commands[++localIterator]);
                 break;
 
             case 'PREVENT_DEFAULT':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.preventDefault(commands[++localIterator]);
+            if (this._webGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
+                this._domRenderer.preventDefault(commands[++localIterator]);
                 break;
 
             case 'ALLOW_DEFAULT':
-                if (this.WebGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
-                this.DOMRenderer.allowDefault(commands[++localIterator]);
+            if (this._webGLRenderer) this.WebGLRenderer.getOrSetCutout(path);
+                this._domRenderer.allowDefault(commands[++localIterator]);
                 break;
 
             case 'GL_SET_DRAW_OPTIONS':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setMeshOptions(path, commands[++localIterator]);
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setMeshOptions(path, commands[++localIterator]);
                 break;
 
             case 'GL_AMBIENT_LIGHT':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setAmbientLightColor(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setAmbientLightColor(
                     path,
                     commands[++localIterator],
                     commands[++localIterator],
@@ -277,8 +303,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_LIGHT_POSITION':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setLightPosition(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setLightPosition(
                     path,
                     commands[++localIterator],
                     commands[++localIterator],
@@ -287,8 +313,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_LIGHT_COLOR':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setLightColor(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setLightColor(
                     path,
                     commands[++localIterator],
                     commands[++localIterator],
@@ -297,8 +323,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'MATERIAL_INPUT':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.handleMaterialInput(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.handleMaterialInput(
                     path,
                     commands[++localIterator],
                     commands[++localIterator]
@@ -306,8 +332,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_SET_GEOMETRY':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setGeometry(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setGeometry(
                     path,
                     commands[++localIterator],
                     commands[++localIterator],
@@ -316,8 +342,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_UNIFORMS':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setMeshUniform(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setMeshUniform(
                     path,
                     commands[++localIterator],
                     commands[++localIterator]
@@ -325,8 +351,8 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_BUFFER_DATA':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.bufferData(
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.bufferData(
                     path,
                     commands[++localIterator],
                     commands[++localIterator],
@@ -337,18 +363,18 @@ Context.prototype.receive = function receive(path, commands, iterator) {
                 break;
 
             case 'GL_CUTOUT_STATE':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setCutoutState(path, commands[++localIterator]);
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setCutoutState(path, commands[++localIterator]);
                 break;
 
             case 'GL_MESH_VISIBILITY':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.setMeshVisibility(path, commands[++localIterator]);
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.setMeshVisibility(path, commands[++localIterator]);
                 break;
 
             case 'GL_REMOVE_MESH':
-                if (!this.WebGLRenderer) this.initWebGL();
-                this.WebGLRenderer.removeMesh(path);
+                if (!this._webGLRenderer) this._initWebGLRenderer();
+                this._webGLRenderer.removeMesh(path);
                 break;
 
             case 'PINHOLE_PROJECTION':
