06a3c9ae202ef1751d5333ebaa32b18df378a951
redwoodfavorite <joseph.michael.sample@gmail.com>
2015-06-03 17:06:00 -0700

    fix: removing removeEventListener from unsubscribe

--- dom-renderers/DOMRenderer.js
+++ dom-renderers/DOMRenderer.js
@@ -129,14 +129,6 @@ DOMRenderer.prototype.subscribe = function subscribe(type, preventDefault) {
 DOMRenderer.prototype.unsubscribe = function unsubscribe(type) {
     this._assertTargetLoaded();
     this._target.subscribe[type] = false;
-
-    if (
-        this._target.listeners[type] || this._root.listeners[type]
-    ) {
-        var target = eventMap[type][1] ? this._root : this._target;
-        target.listeners[type] = null;
-        target.element.removeEventListener(type, this._boundTriggerEvent);
-    }
 };
 
 /**
